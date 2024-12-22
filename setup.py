87cf08e730549ab902358ba0d8f4876519bd28ff
TheSecondLugia <135346993+TheSecondLugia@users.noreply.github.com>
2024-12-22 09:11:33 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from pathlib import Path
 from setuptools import setup
 
 setup(
-    name='pixoo',
-    version='0.9.2',
-    author='Ron Talman',
-    description='A library to easily communicate with the Divoom Pixoo 64',
+    name='pixoo-next',
+    version='0.1',
+    author='TheSecondLugia',
+    description='A library to easily communicate with the Divoom Pixoo 16/64 devices',
     license='CC BY-NC-SA',
-    keywords=['pixoo', 'divoom', 'pixoo64'],
+    keywords=['pixoo', 'divoom', 'pixoo64', 'pixoo16'],
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type='text/markdown',
     url='https://github.com/SomethingWithComputers/pixoo#readme',
