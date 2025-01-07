dccd0b926e9df32dba60e769d8990aadaf884882
TheSecondLugia <135346993+TheSecondLugia@users.noreply.github.com>
2025-01-07 13:45:09 -0800

    Added ItemType enum

--- src/pixoo/__init__.py
+++ src/pixoo/__init__.py
@@ -8,7 +8,8 @@ from .enums.imageresamplemode import ImageResampleMode
 from .enums.textscrolldirection import TextScrollDirection
 from .objects.pixoo import Pixoo
 from .objects.pixoorest import PixooREST
+from .enums.itemtype import ItemType
 
 __all__ = (
-    Channel, ImageResampleMode, Palette, Font, Pixoo, PixooConfiguration, PixooREST, RESTConfiguration,
+    Channel, ImageResampleMode, Palette, Font, ItemType, Pixoo, PixooConfiguration, PixooREST, RESTConfiguration,
     SimulatorConfiguration)
